e2be421f3383872848df5f37058569ecfc4213ed
slava_v23 <25397181+slava-v23@users.noreply.github.com>
2023-12-06 02:47:41 -0800

    Add files via upload

---
+++ Client/posix/html/game/game.js
@@ -0,0 +1,9 @@
+import Ball from './Ball.js'
+
+const ball = new Ball(document.getElementById("ball"))
+
+function update(time) {
+    window.requestAnimationFrame(update)
+}
+
+window.requestAnimationFrame(update)
