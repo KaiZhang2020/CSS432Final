2ef32ed1c9468c5b89992d0c7f277d1ab0347b05
slava_v23 <25397181+slava-v23@users.noreply.github.com>
2023-12-06 02:44:35 -0800

    Update scoreboard.js: unregister/FileHandler(for css+fonts)/added scoreboard(html+css+btn's functionality), optimized with handlers, LAN, default game room

--- Client/posix/html/scoreboard.js
+++ Client/posix/html/scoreboard.js
@@ -10,3 +10,65 @@
 // all games (you can back it up to a file regularly on the server so 
 // if your server crashes, it can automatically reload it from the disk 
 // the next time it restarts).
+const ScoreBoardHTML = () => {
+    return `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Scoreboard</title>
+    <link rel="stylesheet" href="scoreboard.css">
+</head>
+<body>
+    <h3>Pong Game</h3>
+    <h1>Top 15</h1>
+    <section id="list">
+        <section class="list-item">
+            <p class="left">rey</p>
+            <p class="green">100:10</p>
+            <p class="right">terminator</p>
+        </section>
+        <section class="list-item">
+            <p class="left">rey</p>
+            <p class="green">100:10</p>
+            <p class="right">terminator</p>
+        </section>
+        <section class="list-item">
+            <p class="left">rey</p>
+            <p class="green">100:10</p>
+            <p class="right">terminator</p>
+        </section>
+        <section class="list-item">
+            <p class="left">gameMonsterrrrrrrrr</p>
+            <p class="yellow">80:10</p>
+            <p class="right">terminator</p>
+        </section>
+        <section class="list-item">
+            <p class="left">gameMonster</p>
+            <p class="yellow">80:10</p>
+            <p class="right">terminator</p>
+        </section>
+        <section class="list-item">
+            <p class="left">gameMonster</p>
+            <p class="yellow">80:10</p>
+            <p class="right">rey</p>
+        </section>
+        <section class="list-item">
+            <p class="left">rey</p>
+            <p class="red">80:10</p>
+            <p class="right">terminator</p>
+        </section>
+    </section>
+    <button onclick="redirectToMainPage()">Go to lobby</button>
+</body>
+</html>
+<script>
+    const redirectToMainPage = () => {
+        window.location.href = '/';
+    }
+</script>
+`
+}
+
+module.exports = ScoreBoardHTML;
